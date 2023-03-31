9ec50bd7f09f433419fcb5bc767c060bcb087ee9
Rangel Reale <rangelspam@gmail.com>
2023-03-31 16:49:01 -0300

    generation

--- cache/ttlcache/options.go
+++ cache/ttlcache/options.go
@@ -8,6 +8,7 @@ import (
 
 // Option
 
+// +troptgen root
 type Options[K comparable, V any] interface {
 	trcache.IsRootOptions
 	trcache.CallDefaultOptions[K, V]
@@ -52,41 +53,9 @@ func (c *cacheOptions[K, V]) OptDefaultDuration(duration time.Duration) {
 	c.defaultDuration = duration
 }
 
-func WithName[K comparable, V any](name string) trcache.RootOption {
-	return trcache.RootOptionFunc(func(o any) bool {
-		switch opt := o.(type) {
-		case Options[K, V]:
-			opt.OptName(name)
-			return true
-		}
-		return false
-	})
-}
-
-func WithValidator[K comparable, V any](validator trcache.Validator[V]) trcache.RootOption {
-	return trcache.RootOptionFunc(func(o any) bool {
-		switch opt := o.(type) {
-		case Options[K, V]:
-			opt.OptValidator(validator)
-			return true
-		}
-		return false
-	})
-}
-
-func WithDefaultDuration[K comparable, V any](defaultDuration time.Duration) trcache.RootOption {
-	return trcache.RootOptionFunc(func(o any) bool {
-		switch opt := o.(type) {
-		case Options[K, V]:
-			opt.OptDefaultDuration(defaultDuration)
-			return true
-		}
-		return false
-	})
-}
-
 // Cache get options
 
+// +troptgen get
 type GetOptions[K comparable, V any] interface {
 	trcache.IsGetOptions
 	trcache.GetOptions[K, V]
@@ -109,21 +78,9 @@ func (c *getOptions[K, V]) OptTouch(b bool) {
 	c.touch = b
 }
 
-// Cache get options: declarations
-
-func WithGetTouch[K comparable, V any](touch bool) trcache.GetOption {
-	return trcache.GetOptionFunc(func(o any) bool {
-		switch opt := o.(type) {
-		case GetOptions[K, V]:
-			opt.OptTouch(touch)
-			return true
-		}
-		return false
-	})
-}
-
 // Cache set options
 
+// +troptgen set
 type SetOptions[K comparable, V any] interface {
 	trcache.IsSetOptions
 	trcache.SetOptions[K, V]
@@ -142,6 +99,7 @@ func (c *setOptions[K, V]) OptDuration(duration time.Duration) {
 
 // Cache delete options
 
+// +troptgen delete
 type DeleteOptions[K comparable, V any] interface {
 	trcache.IsDeleteOptions
 	trcache.DeleteOptions[K, V]
@@ -152,3 +110,5 @@ type deleteOptions[K comparable, V any] struct {
 }
 
 var _ DeleteOptions[string, string] = &deleteOptions[string, string]{}
+
+//go:generate troptgen

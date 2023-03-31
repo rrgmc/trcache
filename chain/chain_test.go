07d86b5f0c93f88eeed7c03ec2ba91d888fa081b
Rangel Reale <rangelspam@gmail.com>
2023-03-31 17:44:10 -0300

    fix test

--- chain/chain_test.go
+++ chain/chain_test.go
@@ -15,39 +15,7 @@ func TestChain(t *testing.T) {
 
 	mockCache1 := mocks.NewCache[string, string](t)
 	mockCache2 := mocks.NewCache[string, string](t)
-
-	mockCache1.EXPECT().Get(mock.Anything, "a", mock.Anything).Return("", trcache.ErrNotFound)
-	mockCache2.EXPECT().Get(mock.Anything, "a", mock.Anything).Return("", trcache.ErrNotFound)
-
-	mockCache1.EXPECT().Set(mock.Anything, "a", "abc123", mock.Anything).Return(nil)
-	mockCache2.EXPECT().Set(mock.Anything, "a", "abc123", mock.Anything).Return(nil)
-
-	c := NewRefresh[string, string]([]trcache.Cache[string, string]{
-		mockCache1, mockCache2,
-	},
-		trcache.WithCallDefaultRefreshOptions[string, string](trcache.RefreshOpt[string, string]().
-			WithRefreshRefreshFunc(func(ctx context.Context, key string, options trcache.RefreshFuncOptions) (string, error) {
-				return "abc" + options.Data.(string), nil
-			}).
-			WithRefreshData("123"),
-		),
-	)
-
-	value, err := c.GetOrRefresh(ctx, "a")
-	require.NoError(t, err)
-	require.Equal(t, "abc123", value)
-}
-
-func TestChainGetStrategyDefault(t *testing.T) {
-	ctx := context.Background()
-
-	mockCache1 := mocks.NewCache[string, string](t)
-	mockCache2 := mocks.NewCache[string, string](t)
 	mockCache3 := mocks.NewCache[string, string](t)
-
-	// mockCache1.EXPECT().Name().Return("c1")
-	// mockCache2.EXPECT().Name().Return("c2")
-	// mockCache3.EXPECT().Name().Return("c3")
 
 	// first cache will not find
 	mockCache1.EXPECT().Get(mock.Anything, "a", mock.Anything).Return("", trcache.ErrNotFound)
@@ -70,3 +38,36 @@ func TestChainGetStrategyDefault(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "12", value)
 }
+
+func TestChainRefresh(t *testing.T) {
+	ctx := context.Background()
+
+	mockCache1 := mocks.NewCache[string, string](t)
+	mockCache2 := mocks.NewCache[string, string](t)
+
+	// no cache will find
+	mockCache1.EXPECT().Get(mock.Anything, "a", mock.Anything).Return("", trcache.ErrNotFound)
+	mockCache2.EXPECT().Get(mock.Anything, "a", mock.Anything).Return("", trcache.ErrNotFound)
+
+	// refresh will be called
+
+	// all cache will be set
+	mockCache1.EXPECT().Set(mock.Anything, "a", "abc123", mock.Anything).Return(nil)
+	mockCache2.EXPECT().Set(mock.Anything, "a", "abc123", mock.Anything).Return(nil)
+
+	c := NewRefresh[string, string]([]trcache.Cache[string, string]{
+		mockCache1, mockCache2,
+	},
+		trcache.WithCallDefaultRefreshOptions[string, string](trcache.RefreshOpt[string, string]().
+			WithRefreshRefreshFunc(func(ctx context.Context, key string, options trcache.RefreshFuncOptions) (string, error) {
+				return "abc" + options.Data.(string), nil
+			}).
+			WithRefreshData("123"),
+		),
+	)
+
+	value, err := c.GetOrRefresh(ctx, "a")
+	require.NoError(t, err)
+	require.Equal(t, "abc123", value)
+}
+

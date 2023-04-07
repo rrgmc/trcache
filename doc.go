8ff673c1912eb0d9f4efa4f8ecf4f882fbc1c871
Rangel Reale <rangelspam@gmail.com>
2023-04-07 17:03:03 -0300

    docs

--- doc.go
+++ doc.go
@@ -2,6 +2,6 @@
 //
 // The wrappers are highly customizable and allows accessing the underlying cache functionality if desired.
 //
-// A loadable (refresh) implementation is available for all caches, which provides a [RefreshCache].GetOrRefresh which
-// calls a refresh function if the data was not found in [Cache].Get.
+// A loadable (refresh) implementation is available for all caches, which provides a "GetOrRefresh" method which
+// calls a refresh function if the data was not found in "Get".
 package trcache
